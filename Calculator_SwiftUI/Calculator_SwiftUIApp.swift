46d166d481f235829cfe1bcbc79d3424b2ceb827
plusone <zhuozhengyi@icloud.com>
2021-04-27 15:36:49 +0800

    Initial Commit

---
+++ Calculator_SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator_SwiftUI
+//
+//  Created by Zhengyi on 2021/4/27.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
